3cd8af87898dbfa60a2155421c36d3fe58284242
Chiru_07 <saparechirudeep2003@gmail.com>
2024-11-28 23:32:02 +0530

    operators day 2

---
+++ Day5/index.js
@@ -0,0 +1,52 @@
+var a = 5;
+var b = 10;
+var c =undefined;
+var d = 0;
+// console.log(a && b);
+// console.log( !a && d);
+// console.log(a>b && a<b);
+// console.log(a<b && a!=b);
+// console.log(c && a);
+// console.log(c || a);
+// console.log(a>b && b>d && c>b);
+// console.log(a || b);
+// console.log(a && !b);
+
+// if([]==[]){
+//     alert("Hello it's right");
+// }
+// else{
+//     alert("Its wrong"); // it will print this line because two empty arrays cannot be equal
+// }
+
+// if(a>b || c==d ){
+//     console.log("it is true");
+// }
+// else{
+//     console.log("It is false");// Thiis will print because both are false
+// }
+
+// if([] && [] ){
+//     console.log("it is true");//true
+// }
+// else{
+//     console.log("It is false");// Thiis will print because both are false
+// }
+
+// if("" || [] ){
+//     alert("Hello it's right");
+    
+// }
+// else{
+//      alert("Hello it is wrong");
+
+// }
+
+if("" && undefined ){
+    alert("Hello it's right");
+    
+}
+else{
+     alert("Hello it is wrong");
+
+}
