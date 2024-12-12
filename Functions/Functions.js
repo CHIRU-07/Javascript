0edfd89a419d3088d320f9d68870255c8c396d61
Chiru_07 <saparechirudeep2003@gmail.com>
2024-12-12 22:01:46 +0530

    other functions

--- Functions/Functions.js
+++ Functions/Functions.js
@@ -1,10 +1,29 @@
-function Obj(){
-    var obj={
-        Hero_name:"Mahesh Babu",
-        Movie_name:"Guntur karam",
-        Director_name:"Guruji",
-        Plotof_movie:"mass"
-    }
-    console.log(obj)
-}
-Obj()
+// function Obj(){
+//     var obj={
+//         Hero_name:"Mahesh Babu",
+//         Movie_name:"Guntur karam",
+//         Director_name:"Guruji",
+//         Plotof_movie:"mass"
+//     }
+//     console.log(obj)
+// }
+// Obj()
+
+//Arrow function
+// var a=()=>{
+//     console.log("This is arrow function")
+// }
+// a()
+
+//Anonymous function
+// var x=function(){
+//      console.log("This is anonymous function")
+// }
+// x()
+
+//Assigning a named function to a variable is called named-function-expression
+// var x=function abc(){
+//     console.log("This is named function expression")
+// }
+// x()
+
